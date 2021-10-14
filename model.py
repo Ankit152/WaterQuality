34e12ee257ed9e6a1141647ac7f5cd164245b592
Ankit Kurmi <ankitkurmi152@gmail.com>
2021-10-14 15:30:52 +0530

    added model.py

---
+++ model.py
@@ -0,0 +1,78 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense,Flatten,Dropout
+from tensorflow.keras.models import Sequential,load_model
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
+import time
+from tensorflow.keras.optimizers import Adam
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split as tts
+
+
+print("[INFO] All libraries are imported....")
+
+data = pd.read_csv('./assets/waterProcessed.csv')
+print("[INFO] Dataset loaded....")
+y = data['Potability'].values
+x = data.drop('Potability',axis=1)
+y = to_categorical(y)
+print(x.shape,y.shape)
+
+print("[INFO] Dividing the dataset into train and test....")
+xtrain, xtest, ytrain, ytest = tts(x,y,test_size=0.1,random_state=42,stratify=y)
+print(xtrain.shape,ytrain.shape)
+print(xtest.shape,ytest.shape)
+print("[INFO] Dividing is done....")
+
+print("Making the model....")
+
+model = Sequential(name="WaterPotability")
+model.add(Dense(128,input_shape=(9,),activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(2,activation="softmax"))
+
+
+checkpointer = ModelCheckpoint('WaterPotability.h5', save_best_only=True,monitor='val_loss',mode='auto')
+
+print("Model architecture is done....")
+model.compile(loss = "categorical_crossentropy", optimizer="adam",metrics=["accuracy"])
+print("Model compiled....")
+print("Training is starting....")
+start = time.time()
+hist = model.fit(xtrain,ytrain,batch_size=16,epochs=15,validation_data=(xtest, ytest),callbacks=[checkpointer])
+print("Model training is over....")
+print("Total Time taken: ",time.time()-start)
+print("Model saved....")
+
+
+# plotting the figures
+print("Plotting the figures....")
+plt.figure(figsize=(15,10))
+plt.plot(hist.history['accuracy'],c='b',label='train')
+plt.plot(hist.history['val_accuracy'],c='r',label='validation')
+plt.title("Model Accuracy vs Epochs")
+plt.xlabel("EPOCHS")
+plt.ylabel("ACCURACY")
+plt.legend(loc='lower right')
+plt.savefig('./img/accuracy.png')
+
+
+plt.figure(figsize=(15,10))
+plt.plot(hist.history['loss'],c='orange',label='train')
+plt.plot(hist.history['val_loss'],c='g',label='validation')
+plt.title("Model Loss vs Epochs")
+plt.xlabel("EPOCHS")
+plt.ylabel("LOSS")
+plt.legend(loc='upper right')
+plt.savefig('./img/loss.png')
+print("Figures saved in the disk....")
+
+model=load_model("WaterPotability.h5")
+# testing the model
+print("Testing the model....")
+print("The result obtained is...\n")
+model.evaluate(xtest,ytest)
